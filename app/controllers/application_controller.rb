b8cd2fff1e349ebdf717c032844dbcd26ffaca25
kevin robert <kevin.j.robert@gmail.com>
2019-06-04 18:28:46 +0200

    add gift form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
-
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
@@ -16,6 +16,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
@@ -24,7 +28,7 @@ class ApplicationController < ActionController::Base
     if current_user.email == 'anisa.caja@yahoo.com'
       gifts_path
     else
-      stored_location_for(resource)
+      stored_location_for(resource) || root_path
     end
   end
 end
--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,3 +1,6 @@
 class Gift < ApplicationRecord
   belongs_to :user
+
+  mount_uploader :photo, PhotoUploader
+  mount_uploader :video, VideoUploader
 end
