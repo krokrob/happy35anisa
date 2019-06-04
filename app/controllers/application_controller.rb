0748269cbcd54f501e64edfc9f3e7c72c99a6494
kevin robert <kevin.j.robert@gmail.com>
2019-06-04 16:17:50 +0200

    add name to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  # def after_sign_in_path_for(resource)
-
-  # end
+  def after_sign_in_path_for(resource)
+    if current_user.email == 'anisa.caja@yahoo.com'
+      gifts_path
+    else
+      stored_location_for(resource)
+    end
+  end
 end
